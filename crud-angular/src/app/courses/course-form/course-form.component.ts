e7d87cf260c363a50853e912ac6c0ea8ccef7d69
Mateus <mateus.oak99@gmail.com>
2023-01-20 09:17:52 -0300

    Formulário: Salvando os dados com HTTP POST + tratamento de erros

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
@@ -10,7 +13,11 @@ export class CourseFormComponent {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder, 
+    private service: CoursesService,
+    private snackBar: MatSnackBar
+    ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -18,10 +25,18 @@ export class CourseFormComponent {
   }
 
   onSubmit(){
-
+    this.service.save(this.form.value)
+      .subscribe(
+        result => console.log(result), 
+        error => this.onError()
+      );
   }
 
   onCancel(){
-    
+    console.log('onCansel')
+  }
+
+  private onError(){
+    this.snackBar.open('Error saving course','', {duration: 5000})
   }
 }
